c896894687d66c70de247dfe439ab4cca599d8e5
Christina Chan <cchan@brainstation.io>
2016-08-31 14:58:49 -0400

    services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,16 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
   template:`
-    {{myHero.name}}
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
       <li
         *ngFor="let hero of heroes"
-        (click)="onSelect(hero, hero.name)"
+        (click)="onSelect(hero)"
         [class.selected]="hero === selectedHero"
       >
         <span class="badge">{{hero.id}}</span> {{hero.name}}
@@ -82,21 +69,29 @@ const HEROES: Hero[] = [
     body {
       background-color: red;
     }
-  `]
+  `],
+  providers: [HeroService] // hero.service.ts as provider
 })
 
 export class AppComponent{
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  
-  myHero: Hero = {
-    id: 1,
-    name: 'Testing'
-  };
+
+  // make HeroService a private variable/instance
+  constructor(private heroService: HeroService) { }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    // gets heroes when our AppComponent activates
+    this.getHeroes();
   }
 }
 
