3855983a8c630febd0116bf6ed219641f9e01de6
Christina Chan <cchan@brainstation.io>
2016-08-31 13:52:30 -0400

    Angular 2 Tutorial Heroes

--- app/app.component.js
+++ app/app.component.js
@@ -9,13 +9,37 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Heroes';
+        this.heroes = HEROES;
     }
+    AppComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular 2 App</h1>'
+            template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n      <li\n        *ngFor=\"let hero of heroes\"\n        (click)=\"onSelect(hero)\"\n        [class.selected]=\"hero === selectedHero\"\n      >\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    <div *ngIf=\"selectedHero\">\n      <h2>{{selectedHero.name}} details!</h2>\n      <div><label>id: </label>{{selectedHero.id}}</div>\n      <div>\n        <label>name: </label>\n        <input\n          [(ngModel)]=\"selectedHero.name\"\n          placeholder=\"name\">\n      </div>\n    </div>\n  ",
+            styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .heroes {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .heroes li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .heroes li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .heroes li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .heroes .text {\n      position: relative;\n      top: -3px;\n    }\n    .heroes .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
@@ -27,9 +51,9 @@ exports.AppComponent = AppComponent;
  * Each component controls a view (through it's template).
  *
  * @Component 'decorator' function takes metadata object as arg, applied to the class.
- *		selector: CSS selector for the HTML element that represents the component
- * 			Angular creates an instance of AppComponent wherever it see 'my-app' element in HTML
- * 		template: the HTML that defines the component (can include other components here too)
+ *    selector: CSS selector for the HTML element that represents the component
+ *      Angular creates an instance of AppComponent wherever it see 'my-app' element in HTML
+ *    template: the HTML that defines the component (can include other components here too)
  *
  * Component Class, empty right now, expand with properties and app logic. Export this class.
  */ 
